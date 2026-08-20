fdb12db8f2dcc34886c6f8fe2510155d3663a9e8
fez <@>
2026-08-20 15:37:31 +0200

    docker

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM mcr.microsoft.com/dotnet/sdk:10.0 AS build
+
+WORKDIR /src
+
+COPY . .
+
+RUN dotnet restore "PomodoroClocker.sln"
+
+RUN dotnet publish \
+    "PomodoroClocker.Api/PomodoroClocker.Api.csproj" \
+    -c Release \
+    -o /app/publish \
+    /p:UseAppHost=false
+
+
+FROM mcr.microsoft.com/dotnet/aspnet:10.0 AS final
+
+WORKDIR /app
+
+COPY --from=build /app/publish .
+
+ENV ASPNETCORE_URLS=http://0.0.0.0:${PORT}
+
+ENTRYPOINT ["dotnet", "PomodoroClocker.Api.dll"]
--- PomodoroClocker.Api/Program.cs
+++ PomodoroClocker.Api/Program.cs
@@ -9,6 +9,13 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT");
+
+if (!string.IsNullOrEmpty(port))
+{
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+}
+
 var jwtKey = builder.Configuration["Jwt:Key"];
 
 if (string.IsNullOrWhiteSpace(jwtKey))
@@ -48,7 +55,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<PomodoroDbContext>(options =>
 {
-    options.UseSqlite("Data Source=pomodoro.db");
+    options.UseNpgsql(
+        builder.Configuration
+            .GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
@@ -57,14 +66,14 @@ builder.Services.AddScoped<JwtService>();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
+/*using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider
         .GetRequiredService<PomodoroDbContext>();
 
     db.Database.Migrate();
 }
-
+*/
 
 if (app.Environment.IsDevelopment())
 {
